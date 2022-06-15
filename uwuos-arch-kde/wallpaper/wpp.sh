1adf8578138668962dba5f5f04f37f51b08f14bb
Ваше Имя <you@example.com>
2022-06-15 15:53:33 +0300

    a

--- uwuos-arch-kde/wallpaper/wpp.sh
+++ uwuos-arch-kde/wallpaper/wpp.sh
@@ -6,3 +6,4 @@ pip install dbus-python
 
 CRL=$(curl https://raw.githubusercontent.com/pashazz/ksetwallpaper/master/ksetwallpaper.py)
 python -c "$CRL" --file "$PWD/w.png"
+python -c "$CRL" --lock-screen --file "$PWD/w.png"
