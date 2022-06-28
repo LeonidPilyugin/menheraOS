5ee92c6ab38afa148325083c32c954472567fac5
Ваше Имя <you@example.com>
2022-06-28 16:03:31 +0300

    a

--- neco-arch-kde/wallpaper/wpp.sh
+++ neco-arch-kde/wallpaper/wpp.sh
@@ -5,5 +5,5 @@ pip install dbus-python
 
 
 CRL=$(curl https://raw.githubusercontent.com/pashazz/ksetwallpaper/master/ksetwallpaper.py)
+python -c "$CRL" --file "$PWD/wpl.png"  --lock-screen
 python -c "$CRL" --file "$PWD/wp.png"
-python -c "$CRL" --lock-screen --file "$PWD/wpl.png"
