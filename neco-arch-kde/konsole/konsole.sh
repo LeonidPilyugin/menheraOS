d0a3f40356aed1ddba98dd0b6a4bad7c2c215657
Universai <solomatin377@gmail.com>
2022-07-13 15:07:49 +0300

    sudo

--- neco-arch-kde/konsole/konsole.sh
+++ neco-arch-kde/konsole/konsole.sh
@@ -2,5 +2,5 @@
 sudo cp -r src/. /etc/skel
 sudo cp -r kons/* /usr/share/konsole/
 
-cat src/.config/konsolerc > ~/.config/konsolerc
+sudo -u $USER cat src/.config/konsolerc > ~/.config/konsolerc
 # cp kons/* ~/.local/share/konsole/
--- neco-arch-kde/wallpaper/wpp.sh
+++ neco-arch-kde/wallpaper/wpp.sh
@@ -3,5 +3,5 @@ sudo pacman -S python-pip --noconfirm
 pip install dbus-python
 
 CRL=$(curl https://raw.githubusercontent.com/pashazz/ksetwallpaper/master/ksetwallpaper.py)
-python -c "$CRL" --file "$PWD/wpl.png"  --lock-screen
-python -c "$CRL" --file "$PWD/wp.png"
+sudo -u $USER python -c "$CRL" --file "$PWD/wpl.png"  --lock-screen
+sudo -u $USER python -c "$CRL" --file "$PWD/wp.png"
